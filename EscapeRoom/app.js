836525c69bf61850d48e077654e64dd4fbe3ac3a
blong292 <63314288+blong292@users.noreply.github.com>
2020-05-06 17:29:19 -0400

    Add files via upload

---
+++ EscapeRoom/app.js
@@ -0,0 +1,20 @@
+const readlineSync = require('readline-sync');
+
+/*const user = readlineSync.question('whats the question?');
+const name = "Here is your answer"
+    console.log(name)
+
+const userName = readlineSync.question("What is your problem");
+const names = "Nothing!"
+console.log(names) */
+
+
+const game = readlineSync.question("If you put your hand in the hole");
+const action = ("You will die")
+console.log(action)
+
+const games = readlineSync.question("To open the door");
+const action2 = ("You must find the key first");
+console.log(action2)
+
+
